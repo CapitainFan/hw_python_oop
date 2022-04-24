5bf59dee08375b798a802f152a0f05105088ae96
CapitainFan <gudokrulit@mail.ru>
2022-04-24 12:34:05 +0300

    First try.

--- homework.py
+++ homework.py
@@ -143,7 +143,7 @@ def read_package(workout_type: str, data: list) -> Training:
 def main(training: Training) -> str:
     """Главная функция."""
     info = training.show_training_info()
-    print(info.get_message())
+    return print(info.get_message())
 
 
 if __name__ == '__main__':
