fd2d3a1ff41e5e0747f173a9e60d4cabcf6e2b58
CapitainFan <gudokrulit@mail.ru>
2022-04-27 00:27:04 +0300

    3 try.

--- homework.py
+++ homework.py
@@ -17,11 +17,12 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
+        mess = (f'Тип тренировки: {self.training_type}; '
                 f'Длительность: {self.duration:.3f} ч.; '
                 f'Дистанция: {self.distance:.3f} км; '
                 f'Ср. скорость: {self.speed:.3f} км/ч; '
                 f'Потрачено ккал: {self.calories:.3f}.')
+        return mess
 
 
 class Training:
